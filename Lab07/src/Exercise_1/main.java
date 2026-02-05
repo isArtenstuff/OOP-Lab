a9af228b054734a28e6a143dd556aff053519cba
Pathomphum <pratom2549@gmail.com>
2026-02-06 00:24:52 +0700

    didn't done.

---
+++ Lab07/src/Exercise_1/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercise_1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class main {
+    public static void main(String[] args) {
+        CheckingAccount a1 = new CheckingAccount(1000, "62070033", 500);
+        Customer c1 = new Customer();
+        Customer c2 = new Customer("Harry", "Potter");
+        Customer c3 = new Customer("Harry", "Potter", a1);        
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c1.equals(c2));
+        System.out.println(c3.equals(c2));
+    }
+}
